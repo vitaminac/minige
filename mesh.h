b50a57f7a37fe5f945effb7c4c5fc29bc1bf3212
daolao <dalao1002@gmail.com>
2018-11-13 18:43:50 +0100

    initial commit

---
+++ mesh.h
@@ -0,0 +1,31 @@
+#ifndef TriangleMesh_H
+#define TriangleMesh_H
+
+#include <iostream>
+#include <vector>
+#include "vec3.h"
+
+class Triangle {
+public:
+	unsigned int a;
+	unsigned int b;
+	unsigned int c;
+
+	inline Triangle(unsigned int a, unsigned int b, unsigned int c) :a(a), b(b), c(c) {
+	}
+};
+
+class TriangleMesh {
+public:
+	std::vector<Vec3> vertices;
+	std::vector<Triangle> faces;
+
+	void importFromOff(std::istream &is);
+	void saveAsOff(std::ostream & os);
+
+	Vec3 surfaceNormal(Triangle & t) const;
+};
+
+std::ostream& operator << (std::ostream & os, const Triangle& t);
+std::ostream& operator << (std::ostream & os, const TriangleMesh& mesh);
+#endif
---
+++ vec3.cpp
@@ -0,0 +1,56 @@
+#include "vec3.h"
+
+ostream& operator<<(ostream& out, const Vec3& v)
+{
+	out << "(" << v.x << "," << v.y << "," << v.z << ")";
+	return out;
+}
+
+double Vec3::module2() const {
+	return this->x*this->x + this->y*this->y + this->z*this->z;
+}
+
+double Vec3::module() const {
+	return sqrt(this->module2());
+}
+
+Vec3 Vec3::operator+(const Vec3& other) const {
+	return Vec3(this->x + other.x, this->y + other.y, this->z + other.z);
+}
+
+Vec3 Vec3::operator-(const Vec3& other) const {
+	return Vec3(this->x - other.x, this->y - other.y, this->z - other.z);
+}
+
+Vec3 Vec3::operator-() const {
+	return Vec3(-this->x, -this->y, -this->z);
+}
+
+Vec3 Vec3::operator*(double scalar) const {
+	return Vec3(this->x * scalar, this->y * scalar, this->z * scalar);
+}
+
+Vec3 operator*(double scalar, const Vec3 & vector) {
+	return Vec3(vector.x * scalar, vector.y * scalar, vector.z * scalar);
+}
+
+double Vec3::operator*(const Vec3 & other) const {
+	return this->x * other.x + this->y * other.y + this->z * other.z;
+}
+
+Vec3 Vec3::normalize() const {
+	double length = this->module();
+	return Vec3(
+		this->x / length,
+		this->y / length,
+		this->z / length
+	);
+}
+
+Vec3 Vec3::cross(const Vec3 & other) const {
+	return Vec3(
+		this->y * other.z - this->z * other.y,
+		-(this->x * other.z - this->z * other.x),
+		this->x*other.y - this->y*other.x
+	);
+}
